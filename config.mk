5f94b461a47eba45b2e229c463d99a9a593bb07f
seba3567 <sebastian26481@gmail.com>
2020-05-30 20:22:24 -0400

    lawnchair

--- config.mk
+++ config.mk
@@ -5,20 +5,26 @@ LOCAL_PATH := DexOS/user
 
 ifneq ($(DexOS),true)
 PRODUCT_PACKAGES += \
-    OPLauncher2 \
+    Lawnchair \
 
 endif
 
 
 ifneq ($(DexOS),true)
 PRODUCT_PACKAGES += \
-    OPWallpaperResources \
     OPWeather
 endif
 
 ifneq ($(DexOS),true)
 PRODUCT_PACKAGES += \
     OnePlusRecentsProvider
+endif
+
+ifneq ($(DexOS),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-op-launcher.xml \
+    $(LOCAL_PATH)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml \
+
 endif
 
 ifneq ($(DexOS),true)
